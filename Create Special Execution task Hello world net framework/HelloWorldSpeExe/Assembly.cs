b1d795a2db962cd80353624af328b8623a90e1a6
Esteban Andrade <54022838+esteban-andrade@users.noreply.github.com>
2021-04-07 11:47:49 +1000

    Training Special Exe

---
+++ Create Special Execution task Hello world net framework/HelloWorldSpeExe/Assembly.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Tricentis.Automation.Creation.Attributes;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//[assembly: AssemblyTitle("Tutorial")]
+
+//[assembly: AssemblyDescription("Test Tutoriial")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("TRICENTIS Technology & Consulting GmbH")]
+//[assembly: AssemblyProduct("Tutorial")]
+//[assembly: AssemblyCopyright("Copyright © TRICENTIS Technology & Consulting GmbH 2013")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+[assembly: EngineId("TutorialEngine")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+//[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//https://stackoverflow.com/questions/22516400/what-does-the-guid-in-c-sharp-programs-in-the-assemblyinfo-cs
+//[assembly: Guid("8fd00ff2-0527-4fe8-8b6b-8d7a2a6be91b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+
+//   [assembly: AssemblyVersion("1.0.0.0")]
+//  [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Create Special Execution task Hello world net framework/HelloWorldSpeExe/helloworld.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tricentis.Automation.AutomationInstructions.TestActions;
+using Tricentis.Automation.Creation;
+using Tricentis.Automation.Engines;
+using Tricentis.Automation.Engines.SpecialExecutionTasks;
+using Tricentis.Automation.Engines.SpecialExecutionTasks.Attributes;
+
+namespace Tutorial1
+{
+    [SpecialExecutionTaskName("HelloWorld")]
+    public class HelloWorld : SpecialExecutionTask
+    {
+        public HelloWorld(Validator validator) : base(validator)
+        {
+
+        }
+        public override ActionResult Execute(ISpecialExecutionTaskTestAction testAction)
+        {
+            return new PassedActionResult("Hello World Validation");
+        }
+    }
+}
